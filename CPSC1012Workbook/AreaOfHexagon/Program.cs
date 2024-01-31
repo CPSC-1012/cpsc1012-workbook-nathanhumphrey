6112ae2071ffe197b4896b22773152599eb26bf7
Nathan Humphrey <NathanH@nait.ca>
2024-01-30 20:10:57 -0700

    Add Area of Hexagon Project

---
+++ CPSC1012Workbook/AreaOfHexagon/Program.cs
@@ -0,0 +1,28 @@
+﻿//
+// Purpose: To calculate the area of a hexagon
+// Author: Nathan Humphrey
+// Date Modified: Jan 30, 2024
+//
+double area;
+double side;
+
+Console.WriteLine("=== Hexagon Area Calculator ===");
+Console.WriteLine();
+
+Console.Write("Enter the length of the side: ");
+side = double.Parse(Console.ReadLine());
+
+// Calculate the area in chunks
+double top = 3 * Math.Sqrt(3);
+double main = top / 2;
+double sSquared = Math.Pow(side, 2); // side * side
+
+area = main * sSquared;
+
+// Calculate all in one line
+area = ((3 * Math.Sqrt(3)) / 2) * (Math.Pow(side, 2));
+
+Console.WriteLine();
+Console.WriteLine($"The area of the hexagon is {area:0.0000}");
+Console.WriteLine();
+Console.WriteLine("End Program");
