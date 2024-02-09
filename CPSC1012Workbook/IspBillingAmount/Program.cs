95755aae48c6fcbfd9020cf04122255ffcedae62
Nathan Humphrey <NathanH@nait.ca>
2024-02-08 20:51:03 -0700

    Add nested, stacked, and switch programs

---
+++ CPSC1012Workbook/IspBillingAmount/Program.cs
@@ -0,0 +1,58 @@
+﻿char package;
+int hours;
+double billingAmount;
+
+Console.WriteLine("==== ISP Billing Calculator ====");
+Console.WriteLine();
+
+Console.WriteLine("---- Menu of Packages ----");
+Console.WriteLine("[A] Basic Package");
+Console.WriteLine("[B] Intermediate Package");
+Console.WriteLine("[C] Unlimited Package");
+Console.WriteLine("");
+
+Console.Write("Enter customer package: ");
+package = Console.ReadLine().ToUpper()[0]; // always uppercase
+
+switch(package)
+{
+	case 'A':
+		// Carry out the basic package billing calculations
+		Console.WriteLine("Package A selected");
+		break;
+	case 'B':
+		// Carry out the intermediate package billing calculations
+		Console.WriteLine("Package B selected");
+		break;
+	case 'C':
+		// Carry out the unlimited package billing calculations
+		Console.WriteLine("Package C selected");
+		break;
+	default:
+		Console.WriteLine($"Sorry, package {package} does not exist.");
+		break;
+}
+
+// Equivalent if-else for the above switch statement
+//if (package == 'A')
+//{
+
+//}
+//else if (package == 'B')
+//{
+
+//}
+//else if (package == 'C')
+//{
+    
+//}
+//else
+//{
+
+//}
+
+Console.WriteLine();
+
+// TODO: display the bill amount - but only if one was calculated
+
+Console.WriteLine("End Program");
---
+++ CPSC1012Workbook/PosNegOrZeroNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿string output;
+int number;
+
+Console.WriteLine("=== Is Your Number Positive, Negative, or Zero ===");
+Console.WriteLine();
+
+Console.Write("Enter number: ");
+number = int.Parse(Console.ReadLine());
+
+if (number > 0)
+{
+	output = "positive";
+}
+else if (number < 0)
+{
+	output = "negative";
+}
+else
+{
+	output = "zero";
+}
+
+Console.WriteLine();
+Console.WriteLine($"The number is {output}");
