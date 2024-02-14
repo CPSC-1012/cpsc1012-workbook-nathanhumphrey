8855fccfcd9b775a2962b63d52d4a529a52b6636
Nathan Humphrey <NathanH@nait.ca>
2024-02-13 21:02:34 -0700

    Add number guessing with a loop

---
+++ CPSC1012Workbook/GetThreeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿int number;
+int count = 0;
+const int MaxEntries = 3;
+
+Console.WriteLine("==== Get Three Numbers ====");
+Console.WriteLine();
+
+while (count < MaxEntries)
+{
+	Console.Write($"Enter number {count + 1}: ");
+	number = int.Parse(Console.ReadLine());
+	Console.WriteLine($"Your number is {number}");
+
+	// Increment the count
+	count = count + 1;
+	
+	// Alternative increment methods
+	// count += 1; 
+	// count++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Program End");
---
+++ CPSC1012Workbook/RandomNumberWhileLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿const int MaxGuesses = 3;
+int random = Random.Shared.Next(1, 11); // 1 through 10
+int userGuess = 0;
+int count = 0;
+
+// Debug option
+Console.WriteLine($"The random number is: {random}");
+
+Console.WriteLine("=== Number Guessing Game ===");
+Console.WriteLine();
+
+Console.WriteLine($"You get {MaxGuesses} guesses to guess the number.");
+Console.WriteLine();
+
+while (userGuess != random && count < MaxGuesses)
+{
+	Console.Write($"Enter guess #{count + 1}: ");
+	userGuess = int.Parse(Console.ReadLine());
+
+	count += 1;
+}
+
+Console.WriteLine();
+
+if (userGuess == random)
+{
+	Console.WriteLine($"You won! It took {count} guesses.");
+}
+else
+{
+	Console.WriteLine("Sorry, you lost :(");
+}
