6e9dc6661c47665024a6d8c9c4f1f0ef13f60fe3
Nathan Humphrey <NathanH@nait.ca>
2024-03-05 21:12:01 -0700

    Add Intro to Methods Projects

---
+++ CPSC1012Workbook/IntroToMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿// Prompting for input
+
+// Method call
+string firstName = Prompt("Enter first name: ");
+string lastName = Prompt("Enter last name: ");
+
+int age = PromptForInt("Enter your age: ", "Enter valid age.");
+
+Console.WriteLine($"Hello, {firstName} {lastName}.");
+Console.WriteLine($"Looks like you're {age} years old.");
+
+// Method definition
+static string Prompt(string label)
+{
+	Console.Write(label);
+	return Console.ReadLine();
+}
+
+// Method reuse and Exception handling in a method
+static int PromptForInt(string label, string errorMessage = "Sorry, enter a valid integer.")
+{
+	int intInput = 0;
+	bool isValidInt = false;
+
+	do
+	{
+		string stringInput = Prompt(label);
+
+		try
+		{
+			intInput = int.Parse(stringInput);
+			isValidInt = true;
+		}
+		catch
+		{
+			Console.WriteLine(errorMessage);
+		}
+	} while (!isValidInt);
+	
+	return intInput;
+}
---
+++ CPSC1012Workbook/MethodScopeExample/Program.cs
@@ -0,0 +1,15 @@
+﻿int num1 = 2;
+int num2 = 3;
+int a = 5;
+int b = 10;
+
+Console.WriteLine(AddTwoNumbers(num2, num1));
+Console.WriteLine(AddTwoNumbers(a, b));
+Console.WriteLine(AddTwoNumbers(9, 9));
+
+static int AddTwoNumbers(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return sum;
+}
