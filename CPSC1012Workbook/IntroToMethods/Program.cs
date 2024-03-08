8205693a5c3a4c97241d85801911bfa899a46c3f
Nathan Humphrey <NathanH@nait.ca>
2024-03-07 19:17:28 -0700

    Add method comments to IntroToMethods

--- CPSC1012Workbook/IntroToMethods/Program.cs
+++ CPSC1012Workbook/IntroToMethods/Program.cs
@@ -10,6 +10,12 @@ Console.WriteLine($"Hello, {firstName} {lastName}.");
 Console.WriteLine($"Looks like you're {age} years old.");
 
 // Method definition
+
+/// <summary>
+/// Prompt the user for input
+/// </summary>
+/// <param name="label">The prompt label to display</param>
+/// <returns>The input the user entered as a string</returns>
 static string Prompt(string label)
 {
 	Console.Write(label);
@@ -17,6 +23,11 @@ static string Prompt(string label)
 }
 
 // Method reuse and Exception handling in a method
+
+/// <summary>Prompt the user for input as an integer</summary>
+/// <param name="label">The prompt label to display</param>
+/// <param name="errorMessage">A custom error message to display if input is invalid (optional)</param>
+/// <returns>The input the user entered as an int</returns>
 static int PromptForInt(string label, string errorMessage = "Sorry, enter a valid integer.")
 {
 	int intInput = 0;
